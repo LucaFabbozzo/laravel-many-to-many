fb8b34b18c825511da45732f8e940efbc7d6b7a9
LucaFabbozzo <lucafabbozzo@gmail.com>
2023-01-24 10:35:45 -0500

    add tech badge into the show

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -4,7 +4,16 @@
     <div class="container w-75 m-auto text-center pt-2 text">
         <a href="{{route('admin.projects.index')}}" class="btn btn-outline-secondary text-uppercase my-4">Back to the List</a>
         <div class="card m-auto p-2 mb-5" style="width: 70%;">
-         <img style="width: 40%" src="{{asset('storage/'. $project->cover_image)}}" class="card-img-top m-auto" alt="{{$project->image_original_name}}">
+        @if ($project->cover_image)
+            <img style="width: 40%" src="{{asset('storage/'. $project->cover_image)}}" class="card-img-top m-auto" alt="{{$project->image_original_name}}">
+        @endif
+        @if ($project->technologies)
+            <div>
+                @foreach ($project->technologies as $technology)
+                    <span class="badge text-bg-info">{{ $technology->name }}</span>
+                @endforeach
+            </div>
+        @endif
             <div class="card-body">
                 <h4 class="card-title fs-2">{{$project->name}}</h4>
                 <p class="card-title fs-4">{{$project->client_name}}</p>
